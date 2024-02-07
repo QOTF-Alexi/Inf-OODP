637605e41079647db41486eca35538a6952a7d74
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 10:17:06 +0100

    Add c2.2

---
+++ Week01/Lesson02/w01.2.c03/w01.2.c03/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("What is the person's name?");
+string name = Console.ReadLine();
+for (int i = 1; i <= 2; i++)
+{
+    Console.WriteLine("Happy birthday to you!");
+}
+Console.WriteLine("Happy birthday dear " + name + "!");
+Console.WriteLine("Happy birthday to you!");
