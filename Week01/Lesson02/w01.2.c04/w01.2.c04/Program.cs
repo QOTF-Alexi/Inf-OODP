da506d9a415b6c967501b69d1323f766f62b75f4
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 10:37:05 +0100

    Add c2.4

---
+++ Week01/Lesson02/w01.2.c04/w01.2.c04/Program.cs
@@ -0,0 +1,18 @@
+﻿string pinCode = "1234";
+
+for (int i = 3; i >= 0; i--)
+{
+    Console.WriteLine("Enter your PIN");
+    Console.WriteLine(i + " attempts left");
+    string attempt = Console.ReadLine();
+    if (attempt == pinCode)
+    {
+        Console.WriteLine("Correct!");
+        break;
+    }
+    else if (attempt != pinCode && i == 1)
+    {
+        Console.WriteLine("Your pass is confiscated.");
+        break;
+    }
+}
