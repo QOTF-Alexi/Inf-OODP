ad96a8d00cfa10b437fc71f6d2beb3ecd5061f12
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 12:12:50 +0100

    Add c2.9

---
+++ Week01/Lesson02/w01.2.c09/w01.2.c09/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int attack = 50;
+        double critChance = 0.33;
+        int bossHP = 1000;
+
+        Random critGenerator = new Random();
+        while (bossHP > 0)
+        {
+            double critHit = critGenerator.NextDouble();
+            int damage;
+            if (critHit <= critChance) {
+                damage = attack * 2;
+            }
+            else
+            {
+                damage = attack;
+            }
+
+            bossHP -= damage;
+            Console.WriteLine("Boss HP: " + bossHP);
+            Console.WriteLine("damage: " + damage);
+            Console.WriteLine("");
+        }
+
+        Console.WriteLine("Boss defeated");
+    }
+}
