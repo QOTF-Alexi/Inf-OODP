6ecdfff0a7a1e599002e956adabd3be38fcda707
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 11:14:35 +0100

    add c2.6

---
+++ Week01/Lesson02/w01.2.c06/w01.2.c06/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        List<string> todos = new List<string>();
+        PrintOverview(todos);
+        todos.Add("Mow lawn");
+        todos.Add("Pay taxes");
+        PrintOverview(todos);
+        todos.Remove("Mow lawn");
+        todos.Add("Shopping");
+        PrintOverview(todos);
+    }
+
+    static void PrintOverview(List<string> lst)
+    {
+        Console.WriteLine("Amount of tasks: " + lst.Count);
+        foreach (var t in lst)
+        {
+            Console.WriteLine(t);
+        }
+    }
+}
