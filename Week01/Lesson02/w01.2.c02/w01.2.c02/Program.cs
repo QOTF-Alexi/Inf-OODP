9d8cdf87a4190cee1f224b65bc703fadbd648485
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 10:19:34 +0100

    forgot to add cs file

---
+++ Week01/Lesson02/w01.2.c02/w01.2.c02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+        var fileList = new List<string>()
+        {
+            "OODP assignment.docx",
+            "Project Presentation.pptx",
+            "TODO list.xlsx",
+        };
+
+        string whichFileToDelete = fileList[0];
+
+        Console.WriteLine("File selected to delete: " + whichFileToDelete);
+
+        string confirm;
+        do
+        {
+            Console.WriteLine("Really delete this file? (y/n)");
+            confirm = Console.ReadLine();
+        } while (confirm != "y");
+
+        Console.WriteLine("File deleted");
+
+        if (confirm == "y")
+        {
+            fileList.Remove(whichFileToDelete);
+            Console.WriteLine("File deleted");
+        }
+    }
+}
