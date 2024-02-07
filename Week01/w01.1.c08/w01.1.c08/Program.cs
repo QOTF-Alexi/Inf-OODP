38c0bb71b58b0b394a390c3a8423e2d540404ccd
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2024-02-07 08:53:38 +0100

    Add files via upload

---
+++ Week01/w01.1.c08/w01.1.c08/Program.cs
@@ -0,0 +1,26 @@
+﻿int xCoord = 0;
+int yCoord = 0;
+
+Console.WriteLine("What direction would you like to go?\nUp\nDown\nRight\nLeft");
+string choice = Console.ReadLine().ToUpper();
+switch(choice)
+{
+    case "UP":
+        yCoord += 1;
+        break;
+    case "DOWN":
+        yCoord -= 1;
+        break;
+    case "LEFT":
+        xCoord -= 1;
+        break;
+    case "RIGHT":
+        xCoord += 1;
+        break;
+    default:
+        Console.WriteLine("Invalid direction");
+        break;
+}
+
+Console.WriteLine("Current position");
+Console.WriteLine("X:" + xCoord + ", Y:" + yCoord);
