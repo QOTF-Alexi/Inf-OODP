c651826bf7cd448951dd76a3b7e808b8455b4582
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 10:05:35 +0100

    Add c2.1

--- Week01/Lesson02/w01.2.c01/w01.2.c01/Program.cs
+++ Week01/Lesson02/w01.2.c01/w01.2.c01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int money = 4;
+while (money > 0)
+{
+    Console.WriteLine("Money left: " + money);
+    Console.WriteLine("Look around (1) or go in a ride (2)?");
+    string choice = Console.ReadLine();
+    switch (choice) {
+        case "1":
+            Console.WriteLine("Yay!");
+            break;
+        case "2":
+            Console.WriteLine("Wheee!");
+            money -= 1;
+            break;
+    }
+}
+Console.WriteLine("Time to go home");
