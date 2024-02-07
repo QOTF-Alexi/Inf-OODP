91404abdd3e81515acf308d23ad0d5733244c778
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 11:45:46 +0100

    Add c1.8

---
+++ Week01/Lesson02/w01.2.c08/w01.2.c08/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Random knownNumberGenerator = new Random(1);
+        for (int i = 0; i <= 10; i++)
+        {
+            int dieRoll = knownNumberGenerator.Next(1, 7);
+            Console.WriteLine(dieRoll);
+        }
+    }
+}
