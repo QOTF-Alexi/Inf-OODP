5667cdfe9b127a4b2f30550fb093b1ac2cb5ae91
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 11:40:35 +0100

    Add c2.7

---
+++ Week01/Lesson02/w01.2.c07/w01.2.c07/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Random randomGenerator = new Random();
+        for (int i = 0; i <= 10; i++)
+        {
+            int dieRoll = randomGenerator.Next(1, 7);
+            Console.WriteLine(dieRoll);
+        }
+    }
+}
