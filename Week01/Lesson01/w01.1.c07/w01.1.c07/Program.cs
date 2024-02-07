21b97b0dd4745b685e8ca8e32001fe88951483f9
QOTF-Alexi <an.prins@protonmail.com>
2024-02-07 09:51:29 +0100

    Add files back in

---
+++ Week01/Lesson01/w01.1.c07/w01.1.c07/Program.cs
@@ -0,0 +1,21 @@
+﻿string word = "Length";
+Console.WriteLine("You have one chance to guess this six-letter word:");
+Console.WriteLine("Le..th");
+string guess = Console.ReadLine();
+
+if (word == guess)
+{
+    Console.WriteLine("Correct!");
+}
+else if (word.ToLower() == guess || word.ToUpper() == guess)
+{
+    Console.WriteLine("Kind of correct; the case was just wrong");
+}
+else if (guess.Length > 6 || guess.Length < 6)
+{
+    Console.WriteLine("Incorrect! That is not even a six-letter word!");
+}
+else
+{
+    Console.WriteLine("Incorrect!");
+}
---
+++ Week01/Lesson01/w01.1.c09/w01.1.c09/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter an age:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+string result = age switch
+{
+    >= 0 and <= 12 => "Age " + age + ": a child",
+    >= 13 and <= 19 => "Age " + age + ": a teenager",
+    >= 20 and <= 150 => "Age " + age + ": an adult",
+    _ => "Age " + age + ": invalid"
+};
+
+Console.WriteLine(result);
