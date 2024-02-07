e39f91544db2192c9e911f5d8fe1b13765339a50
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2024-02-07 08:50:38 +0100

    Add files via upload

---
+++ Week01/w01.1.c05/w01.1.c05/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("What is the temperature of the water? (Celsius)");
+double temp = Convert.ToDouble(Console.ReadLine());
+if (temp <= 0)
+{
+    Console.WriteLine("At " + temp + " degrees Celsius, the water will be solid");
+}
+else if ((temp > 0) && (temp < 100))
+{
+    Console.WriteLine("At " + temp + " degrees Celsius, the water will be liquid");
+}
+else if (temp >= 100)
+{
+    Console.WriteLine("At " + temp + " degrees Celsius, the water will be gas");
+}
+else
+{
+    Console.WriteLine("Invalid temperature");
+}
---
+++ Week01/w01.1.c06/w01.1.c06/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Give some text");
+string text = Console.ReadLine();
+
+Console.WriteLine("What do you want to do with this text?");
+Console.WriteLine("U: make all uppercase");
+Console.WriteLine("L: make all lowercase");
+Console.WriteLine("Any other key: do not change");
+string choice = Console.ReadLine().ToUpper();
+
+string newText = choice switch
+{
+    "U" => text.ToUpper(),
+    "L" => text.ToLower(),
+    _ => text
+};
+
+Console.WriteLine(newText);
