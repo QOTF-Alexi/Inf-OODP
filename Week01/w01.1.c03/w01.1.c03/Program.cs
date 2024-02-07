136c2b9309db8661e25a9827ae75d6b2d2f88b93
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2024-02-07 08:48:41 +0100

    Add files via upload

---
+++ Week01/w01.1.c03/w01.1.c03/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("What is the temperature in Celsius?");
+double celsius = Convert.ToDouble(Console.ReadLine());
+double fahrenheit = celsius * 1.8 + 32;
+Console.WriteLine((celsius + " C = " + fahrenheit + " F"));
+Console.WriteLine(("Truncated that is " + Convert.ToInt32(Math.Truncate(fahrenheit)) + " F"));
