ac2735c65c41cde2f0e34ae8018ac36be34c14a0
QOTF-Alexi <an.prins@protonmail.com>
2024-02-08 14:50:27 +0100

    Add part of homework

---
+++ Week01/Lesson02/w01.2.h01/w01.2.h01/Program.cs
@@ -0,0 +1,58 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("What is the amount to pay?");
+        int toPay = Convert.ToInt32(Console.ReadLine());
+        int start = toPay;
+        while (toPay > 0)
+        {
+            Console.WriteLine("\n" + toPay + " left to pay");
+            Console.WriteLine("Pay how much?");
+            Console.WriteLine("1: 5\n2: 10\n3: 20\n4: 50");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    toPay -= 5;
+                    break;
+                case "2":
+                    toPay -= 10;
+                    break;
+                case "3":
+                    toPay -= 20;
+                    break;
+                case "4":
+                    toPay -= 50;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
+
+        if (toPay < 0)
+        {
+            string tipMe = "";
+            do
+            {
+                Console.WriteLine("You paid " + toPay * -1 + " too much. Give a tip? y/n");
+                tipMe = Console.ReadLine().ToLower();
+            } while (tipMe is not ("y" or "n"));
+            if (tipMe == "y")
+            {
+                int tipped = start + (toPay * -1);
+                Console.WriteLine("You have paid " + tipped);
+            }
+            else
+            {
+                Console.WriteLine("You have paid " + start);
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("You have paid " + start);
+        }
+    }
+}
---
+++ Week01/Lesson02/w01.2.h02/w01.2.h02/Program.cs
@@ -0,0 +1,35 @@
+﻿// V Your code goes below V
+string colour = "";
+int chosen_num = 0;
+
+do
+{
+    Console.WriteLine("Pick a color (red/blue/green/yellow):");
+    colour = Console.ReadLine();
+} while (colour is not ("red" or "blue" or "green" or "yellow"));
+int colourWordLength = colour.Length;
+
+do
+{
+    Console.WriteLine("Pick a number (1-8):");
+    chosen_num = Convert.ToInt32(Console.ReadLine());
+} while (chosen_num > 8 || chosen_num < 1);
+
+int fortuneNumber = (colourWordLength + chosen_num) % 4 + 1;
+
+// ^ Your code goes above ^
+//No need to look below here for this assignment. But you are welcome to!
+PrintFortune(fortuneNumber);
+
+static void PrintFortune(int fortuneNumber)
+{
+    string fortune = fortuneNumber switch
+    {
+        1 => "You will be loved and be happy!",
+        2 => "You will be loved and be rich!",
+        3 => "You will be loved and be famous!",
+        4 => "You will be loved, and you'll be happy, rich and famous!",
+        _ => "Unknown. :( But you will still be loved, no matter what."
+    };
+    Console.WriteLine(fortune);
+}
