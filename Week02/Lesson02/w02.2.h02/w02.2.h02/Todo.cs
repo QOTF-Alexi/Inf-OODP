e1a9c93ec1f8eceaf9fe7971d6f8f0051e4c98ff
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2024-02-19 14:31:48 +0100

    Add files via upload

---
+++ Week02/Lesson02/w02.2.h02/w02.2.h02/Todo.cs
@@ -0,0 +1,29 @@
+﻿class Todo
+{
+    List<Task> TaskList = new List<Task>();
+
+    public void AddTask(string Name)
+    {
+        TaskList.Add(new Task(Name));
+    }
+
+    public Task GetTask(string SearchTerm)
+    {
+        foreach (Task t in TaskList)
+        {
+            if (t.Name == SearchTerm)
+            {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public void Report()
+    {
+        foreach (Task t in TaskList)
+        {
+            Console.WriteLine(t.Info());
+        }
+    }
+}
