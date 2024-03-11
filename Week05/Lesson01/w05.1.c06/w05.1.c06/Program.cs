e060d3961401111d20e2e56dc9255c72dba797d5
QOTF-Alexi <an.prins@protonmail.com>
2024-03-11 10:29:44 +0100

    Add second 3rd of w05.1.c

---
+++ Week05/Lesson01/w05.1.c06/w05.1.c06/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Retrieving employees from a database or a file
+        List<object> employees = new()
+        {
+            new Employee(1, "Miles Dyson", "Developer"),
+            new Employee(2, "Coleman Reese", "Lawyer"),
+            new Manager (3, "Bill Lumbergh", "Manager", 5),
+            new Manager (4, "Michael Scott", "Manager", 10),
+            new Dog("Owney the mascot dog") //Not an employee
+        };
+        ListDirectReports(employees);
+    }
+
+    public static void ListDirectReports(List<object> employees)
+    {
+        foreach (var e in employees)
+        {
+            Manager mgr = e as Manager;
+            if (mgr != null)
+            {
+                Console.WriteLine($"Manager {mgr.Name} has {mgr.DirectReportsCount} direct reports.");
+            }
+        }
+    }
+}
