c99e7f730affb5ce0962002c8c62763d5d849fee
QOTF-Alexi <an.prins@protonmail.com>
2024-03-15 12:30:03 +0100

    nope

---
+++ Week05/Lesson02/w05.2.h02/w05.2.h02/RangedWeapon.cs
@@ -0,0 +1,9 @@
+﻿public class RangedWeapon : Weapon
+{
+    public readonly int Range;
+
+    public RangedWeapon(string name, int damage, int range) : base(name, damage)
+    {
+        Range = range;
+    }
+}
---
+++ Week05/Lesson02/w05.2.h02/w05.2.h02/Ranger.cs
@@ -0,0 +1,27 @@
+﻿public class Ranger : Fighter
+{
+    public int Arrows;
+    
+    public Ranger(string name, Weapon weapon) : base(name, weapon)
+    {
+
+    }
+
+    public Ranger(string name, RangedWeapon rangedWeapon, int amountArrows) : base(name, rangedWeapon)
+    {
+        Arrows = amountArrows;
+    }
+
+    public override int Attack()
+    {
+        if (MainWeapon.GetType() == typeof(RangedWeapon) && Arrows > 0)
+        {
+            return BaseAttack + MainWeapon.Damage;
+        }
+        else if (MainWeapon.GetType() == typeof(Weapon))
+        {
+            return Convert.ToInt32((BaseAttack + MainWeapon.Damage) * 0.75);
+        }
+        else return 0;
+    }
+}
