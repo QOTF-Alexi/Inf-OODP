8b03283c1eaa26f8eee58d33a79478e52d366c9d
QOTF-Alexi <an.prins@protonmail.com>
2024-03-15 11:23:37 +0100

    Fuck programming honestly

---
+++ Week05/Lesson02/w05.2.h01/w05.2.h01/Publication.cs
@@ -0,0 +1,47 @@
+﻿public class Publication
+{
+    public string Publisher { get; set; }
+    public string Title { get; set; }
+    public string PublicationType { get; set; }
+    private int _pages;
+    public int Pages
+    {
+        get => _pages;
+        set
+        {
+            if (value > 0)
+            {
+                _pages = value;
+            }
+        }
+    }
+    public DateTime PublicationDate { get; set; }
+    public virtual string PublishedOn
+    {
+        get => IsPublished ? PublicationDate.ToString("yyyy-MM-dd") : "not published yet";
+    }
+
+    protected List<string> Audience { get; set; }
+    protected bool IsPublished
+    {
+        get => PublicationDate.ToString("yyyy-MM-dd") != "0001-01-01";
+    }
+
+    public Publication(string title, string publisher, string pubType, List<string> audience)
+    {
+        Publisher = publisher;
+        Title = title;
+        PublicationType = pubType;
+        Audience = audience;
+    }
+
+    public bool IsSuitableForAudience(string audience)
+    {
+        return Audience.Contains(audience);
+    }
+
+    public override string ToString()
+    {
+        return $"{Title}, {Pages} pages, {PublishedOn}";
+    }
+}
