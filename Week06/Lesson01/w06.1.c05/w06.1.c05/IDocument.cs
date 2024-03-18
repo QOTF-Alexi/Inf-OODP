d97ac8940a6ef3df60d7679768e24ccdb008cad1
QOTF-Alexi <an.prins@protonmail.com>
2024-03-18 10:45:33 +0100

    add rest of w06.1 class assignments

---
+++ Week06/Lesson01/w06.1.c05/w06.1.c05/IDocument.cs
@@ -0,0 +1,8 @@
+public interface IDocument : IStorable
+{
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public void Print();
+}
---
+++ Week06/Lesson01/w06.1.c07/w06.1.c07/IStorable.cs
@@ -0,0 +1,8 @@
+public interface IStorable
+{
+    public string FileName { get; set; }
+
+    public void Load();
+
+    public void Save();
+}
