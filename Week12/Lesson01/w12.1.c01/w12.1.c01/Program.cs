748e8cb1573dcffee6e973be2e6c89e392451fc5
QOTF-Alexi <an.prins@protonmail.com>
2024-05-06 10:06:53 +0200

    Add W12.1 class assignments

---
+++ Week12/Lesson01/w12.1.c01/w12.1.c01/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number");
+        var num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(RecursiveSum(num));
+    }
+
+    public static int RecursiveSum(int n)
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+
+        return n + RecursiveSum(n - 1);
+    }
+}
---
+++ Week12/Lesson01/w12.1.c02/w12.1.c02/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number");
+        var num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Factorial(num));
+    }
+
+    public static int Factorial(int n)
+    {
+        if (n <= 0)
+        {
+            return -1;
+        }
+
+        return n * Factorial(n - 1);
+    }
+}
